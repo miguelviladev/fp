9ba6159d249b0ea5bbe10a444236591f9720c7d4
Miguel Vila <miguel.vila.dev@gmail.com>
2021-11-07 10:23:13 +0100

    exec7

--- lecture06/exec7.py
+++ lecture06/exec7.py
@@ -1,5 +1,9 @@
 import os, sys
 
+# Poderão existir incompatibilidades no que toca ao formato dos paths dependendo do sistema operativo que se use. (no meu caso uso linux)
+
+# Este script exige que se passe um argumento quando se interpreta o mesmo. Assim deve-se executálo desta forma: "python3 exec7.py caminho_do_diretorio"
+
 def main():
     try:
         dir_list = os.listdir(sys.argv[1])
